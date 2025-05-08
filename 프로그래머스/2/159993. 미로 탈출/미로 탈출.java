c91e53238686ad017011b1deb72cd80ba24b8190
hihellosj <135914187+hihellosj@users.noreply.github.com>
2025-05-09 00:28:28 +0900

    [level 2] Title: 미로 탈출, Time: 5.56 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,82 +1,87 @@
 import java.util.*;
 
 class Solution {
-    private static final int [] dx = {0,0,1,-1};
-    private static final int [] dy = {1,-1,0,0};
+    private static int[] dx = {0,0,1,-1};
+    private static int[] dy = {1,-1,0,0};
     
-    private static class Point {
-        int nx, ny;
-        
-        public Point(int nx, int ny) {
-            this.nx = nx;
-            this.ny = ny;
+    private class Point{
+        int x;
+        int y;
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
         }
     }
+    
     private static char[][] map;
-    private static int N,M;
+    private static int N,M;  
     
     public int solution(String[] maps) {
         N = maps.length;
         M = maps[0].length();
         
         map = new char[N][M];
+        
         for (int i = 0 ; i<N; i++) {
             map[i] = maps[i].toCharArray();
         }
-        Point start = null, end = null, lever = null;
         
-        for (int i = 0; i < N ; i ++) {
-            for (int j = 0; j <M; j++){
-                if (map[i][j] == 'S') {
-                    start = new Point(j, i);
-                }else if(map[i][j] == 'E') {
-                    end = new Point(j, i);
-                }else if(map[i][j] == 'L'){
-                    lever = new Point(j, i);
+        Point start = null, end = null, leber = null;
+        
+        for(int i = 0; i < N ; i++) {
+            for(int j = 0; j < M; j++) {
+                if(map[i][j]=='S'){
+                    start = new Point(i, j);
+                }else if(map[i][j]=='L'){
+                    leber = new Point(i, j);
+                }else if(map[i][j]=='E'){
+                    end = new Point(i, j);
                 }
             }
-            }
-            
-            int startLever = bfs(start, lever);
-            int leverEnd = bfs(lever, end);
-            
-            if(startLever == -1 || leverEnd == -1)
-                return -1;
-            else
-                return startLever + leverEnd;
-    }
-        
-        private static int bfs(Point start, Point end){
-            int[][] dist = new int[N][M];
-            ArrayDeque<Point> queue = new ArrayDeque<>();
-            dist[start.ny][start.nx] = 1;
-            queue.add(start);
-            
-            while (!queue.isEmpty()) {
-                Point now = queue.poll();
-                
-                for(int i = 0; i < 4; i++) {
-                    int nextX = now.nx + dx[i];
-                    int nextY = now.ny + dy[i];
-                    
-                    if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) 
-                        continue;
-                    
-                     if(dist[nextY][nextX] > 0)
-                       continue;
-                    
-                    if(map[nextY][nextX] == 'X')
-                        continue;
-                    
-                    dist[nextY][nextX] = dist[now.ny][now.nx] + 1;
-                    
-                    queue.add(new Point(nextX, nextY));
-                    
-                    if(nextX == end.nx && nextY == end.ny)
-                        return dist[end.ny][end.nx] -1;
-                    
-                }
-            }
-            return -1;
         }
+        
+        int startLeber = bfs(start, leber);
+        int leberEnd = bfs(leber, end);
+        
+        
+         return startLeber != -1 && leberEnd != -1 ? startLeber+leberEnd : -1;
+    }
+    
+    private int bfs(Point start, Point end){
+        int[][] dist = new int[N][M];
+        ArrayDeque<Point> queue = new ArrayDeque<>();
+        dist[start.x][start.y] = 1;
+        queue.add(start);
+        
+        while(!queue.isEmpty()){
+            Point now = queue.poll();
+            
+            for(int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+                
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) 
+                    continue;
+                
+                if(map[nx][ny] == 'X')
+                    continue;
+                
+                if(dist[nx][ny] > 0)
+                    continue;
+                
+                dist[nx][ny] = dist[now.x][now.y] + 1;
+                queue.add(new Point(nx, ny));
+                
+                if(nx == end.x && ny == end.y) {
+                    return dist[end.x][end.y] -1;
+                }
+                
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+    
 }
