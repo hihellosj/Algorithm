b4624177f78836a91d8062f6abdedd2e6e4a7ecd
hihellosj <135914187+hihellosj@users.noreply.github.com>
2025-05-12 22:36:45 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,20 +1,7 @@
-
-
-select cart_id
-from(
-select cart_id, sum(cnt2) as total
-from(
-SELECT distinct cart_id, name,'1' as cnt2
-from CART_PRODUCTS
-where name = 'Yogurt'
-union all
-SELECT distinct cart_id, name,'1' as cnt
-from CART_PRODUCTS
-where name = 'Milk'
-    ) t
-group by cart_id
-) t2
-where total > 1
-order by cart_id
-
+SELECT cart_id
+FROM cart_products
+WHERE name IN ('Milk', 'Yogurt')
+GROUP BY cart_id
+HAVING COUNT(DISTINCT name) = 2
+ORDER BY cart_id;
 
