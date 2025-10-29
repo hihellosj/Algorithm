deb496b0a853d45e82c4257e7d4b4df4738d8b5d
hihellosj <135914187+hihellosj@users.noreply.github.com>
2025-10-29 20:50:39 +0900

    [Silver V] Title: 방 번호, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -2,16 +2,14 @@ import math
 
 N = input().strip()
 
-# 숫자 0~9 개수 저장할 리스트
 counts = [0] * 10
 
 for c in N:
     num = int(c)
-    if num == 9:  # 9는 6으로 취급
+    if num == 9:
         num = 6
     counts[num] += 1
-
-# 6은 9 포함해서 반 올림
+    
 counts[6] = math.ceil(counts[6] / 2)
 
 print(max(counts))
