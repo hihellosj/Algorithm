c422e31fa4aac9baa709b0ed2e933ec6b504c7b5
hihellosj <135914187+hihellosj@users.noreply.github.com>
2025-12-22 22:03:59 +0900

    [level 0] Title: 문자열 묶기, Time: 5.50 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int[] count= new int[31];
+        
+        for(String s : strArr) {
+            count[s.length()]++;
+        }
+        int max = 0;
+        for(int c : count) {
+            if(c > max) max = c;
+        }
+            
+        return max;
+    }
+}
