249c9e17431ed7d8a20f4adad56295fb0657ecd9
hihellosj <135914187+hihellosj@users.noreply.github.com>
2025-12-27 22:16:55 +0900

    [level 0] Title: 코드 처리하기, Time: 8.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;  // 시작은 0
+
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+            
+            if (c == '1') {
+                mode = 1 - mode;  // 0 ↔ 1 토글
+            } else {
+                if ((mode == 0 && i % 2 == 0) || (mode == 1 && i % 2 == 1)) {
+                    ret.append(c);
+                }
+            }
+        }
+        
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
+    }
+}
