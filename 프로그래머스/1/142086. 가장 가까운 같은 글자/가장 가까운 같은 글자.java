5baf811796f7735c372e3ea21684cf078d0677ce
hihellosj <135914187+hihellosj@users.noreply.github.com>
2026-01-03 12:10:57 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.80 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] result = new int[s.length()];
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(!lastIndex.containsKey(c)) {
+                result[i] = -1;
+            } else {
+                result[i] = i - lastIndex.get(c);
+            }
+            lastIndex.put(c, i);
+        }
+        return result;
+    }
+}
