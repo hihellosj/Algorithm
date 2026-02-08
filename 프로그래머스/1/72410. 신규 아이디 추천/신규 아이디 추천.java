b2c7b06af3cc97339b7ca4d7492515e289f6ca8b
hihellosj <135914187+hihellosj@users.noreply.github.com>
2026-02-08 15:56:44 +0900

    [level 1] Title: 신규 아이디 추천, Time: 12.66 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String new_id) {
+        String id = new_id.toLowerCase();
+        
+        id = id.replaceAll("[^a-z0-9\\-_.]", "");
+        
+        id = id.replaceAll("\\.{2,}", ".");
+        
+        id = id.replaceAll("^\\.|\\.$", "");
+        
+        if (id.isEmpty()) {
+            id = "a";
+        }
+        
+        if (id.length() >= 16) {
+            id = id.substring(0, 15);
+            id = id.replaceAll("\\.$", "");
+        }
+        
+        while (id.length() < 3) {
+            id += id.charAt(id.length() - 1);
+        }
+        return id;
+    }
+}
