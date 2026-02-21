3621678423afb66ac85c6f32d11c6eb173dd7b3e
hihellosj <135914187+hihellosj@users.noreply.github.com>
2026-02-21 19:01:36 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(long n) {
-        // 문자열로 변환 후 뒤집어서 각 자리수 추출
-        String s = Long.toString(n);
-        int[] answer = new int[s.length()];
+        List<Integer> list = new ArrayList<>();
         
-        for (int i = 0; i < s.length(); i++) {
-            // 뒤에서부터 꺼내어 배열에 넣기
-            answer[i] = s.charAt(s.length() - 1 - i) - '0';
+        while (n > 0) {
+            list.add((int)(n % 10)); // 마지막 자리수 추가
+            n /= 10;                  // 마지막 자리 제거
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
         
         return answer;
