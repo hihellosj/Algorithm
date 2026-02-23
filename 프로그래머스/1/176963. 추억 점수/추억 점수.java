e3fa52f0919f4674c2670372ec691da9ec2b0cba
hihellosj <135914187+hihellosj@users.noreply.github.com>
2026-02-23 20:03:58 +0900

    [level 1] Title: 추억 점수, Time: 2.28 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        Map<String, Integer> scoreMap = new HashMap<>();
+        Map<String, Integer> yn = new HashMap<>();
+        
         for(int i = 0; i < name.length; i++) {
-            scoreMap.put(name[i], yearning[i]);
+            yn.put(name[i], yearning[i]);
         }
         
         int[] result = new int[photo.length];
         
-        for(int i = 0; i < photo.length; i++) {
+        for (int j = 0; j < photo.length; j++) {
             int sum = 0;
-            for(String person : photo[i]) {
-                sum += scoreMap.getOrDefault(person, 0);
+            for(String pn : photo[j]) {
+                
+                sum += yn.getOrDefault(pn, 0);
+        
             }
-            result[i] = sum;
+            result[j]  = sum;
         }
         return result;
     }
